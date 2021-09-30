73a6924bf0ead43b693ed738ec7484c7aec5bf9f
Nilton Guilhem Junior <niltonjuniorzup@zup.com.br>
2021-09-30 19:52:44 -0300

    implemente crud

--- src/main/java/com/crud/java/core/port/ProductServicePort.java
+++ src/main/java/com/crud/java/core/port/ProductServicePort.java
@@ -3,6 +3,10 @@ package com.crud.java.core.port;
 import com.crud.java.core.model.Product;
 import com.crud.java.entrypoint.dto.ProductDto;
 
+import java.util.List;
+
 public interface ProductServicePort {
     ProductDto saveProduct(Product product);
+    List<ProductDto> findAllProduct();
+    void deleteProduct(Long id);
 }
--- src/main/java/com/crud/java/database/service/ProductEntityService.java
+++ src/main/java/com/crud/java/database/service/ProductEntityService.java
@@ -7,6 +7,9 @@ import com.crud.java.database.repository.ProductEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProductEntityService implements ProductEntityServicePort {
 
@@ -14,10 +17,22 @@ public class ProductEntityService implements ProductEntityServicePort {
     private ProductEntityRepository productEntityRepository;
 
     @Override
+    public List<Product> findAll() {
+        return productEntityRepository.findAll().stream().map(entity -> new Product(
+                entity.getId(),entity.getName(),entity.getPrice(),entity.getDescription()
+        )).collect(Collectors.toList());
+    }
+
+    @Override
     public Product saveProductEntity(ProductEntity productEntity) {
         var productRepository = productEntityRepository.save(productEntity);
 
         return new Product(productRepository.getId(),productRepository.getName(),productRepository.getPrice(),
                 productRepository.getDescription());
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productEntityRepository.deleteById(id);
     }
 }
